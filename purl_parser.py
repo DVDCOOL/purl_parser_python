8569a58ffc101fa3c4ee52efa296aeebc38e0c05
obsulite <huegelig.beschattet.4s@icloud.com>
2025-10-08 09:55:25 +0200

    Unnötige Variablen etc. entfernt

--- purl_parser.py
+++ purl_parser.py
@@ -2,47 +2,36 @@
 #nimmt einen purl entgegen und gibt die Einzelteile als Liste aus
 #scheme:title/namespace/name@version?qualifiers#subpath
 def parse(purl):
-    _purl = purl
-    _purlSchema = _purl[:4]
-    _purlURL = _purl[4:]
+    _purlSchema = purl[:4]
+    _purlURL = purl[4:]
     _purlKomponenten = _purlURL.split("/")
 
-    if not istRichtigerPurl(_purlSchema, _purlKomponenten):
-        print("Invalider PURL")
-    else:
-        geparsterPurl = teileInKomponenten(_purlKomponenten)
-        print(*geparsterPurl, sep= ", ")
-        return geparsterPurl
- 
+    if istRichtigerPurl(_purlSchema, _purlKomponenten):
+        return teileInKomponenten(_purlKomponenten)
+
 #Überprüft ob die Anforderungen an ein PURL gegeben sind
 def istRichtigerPurl(schema, komp):
-    _schema = schema
-    _komp = komp
-    return _schema == "pkg:" and len(_komp) >= 2
+    return schema == "pkg:" and len(komp) >= 2
 
 #Teilt den Purl in die einzelnen Komponenten
 def teileInKomponenten(komponenten):
-    _komponenten = komponenten
-    _listeKomponenten = []
-    _listeKomponenten.append(komponenten[0])
-    if len(_komponenten) == 3:
-        _listeKomponenten.append(_komponenten[1])
-    _listeKomponenten.extend(teileInOptionaleKomponenten(_komponenten[-1]))
+    _listeKomponenten = [komponenten[0]]
+    if len(komponenten) == 3:
+        _listeKomponenten.append(komponenten[1])
+    _listeKomponenten.extend(teileInOptionaleKomponenten(komponenten[-1]))
     return _listeKomponenten
 
 #Teilt, wenn vorhanden, die optionalen Komponenten aus dem Purl heraus
 def teileInOptionaleKomponenten(string):
     _komponentenliste = []
     _string = string
-    if "#" in string:
-        _komponentenliste.append(_string.split("#")[1])
-        _string = _string.split("#")[0]
-    if "?" in string:
-        _komponentenliste.append(_string.split("?")[1])
-        _string = _string.split("?")[0]
-    if "@" in string:
-        _komponentenliste.append(_string.split("@")[1])
-        _string = _string.split("@")[0]
+    _separators = ["#", "?", "@"]
+
+    for i in range(len(_separators)):
+        if _separators[i] in _string:
+            _komponentenliste.append(_string.split(_separators[i])[1])
+            _string = _string.split(_separators[i])[0]
+    
     _komponentenliste.append(_string)
     _komponentenliste.reverse()
     return(_komponentenliste)
@@ -56,5 +45,4 @@ def main():
     parse("pkg:asdafdssdfjhsdfkj")
 
 if __name__ == "__main__":
-    main()
-
+    main()  
