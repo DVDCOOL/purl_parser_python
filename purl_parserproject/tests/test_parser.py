a9a21b02612e39989b875283f493b678917aa14d
obsulite <huegelig.beschattet.4s@icloud.com>
2025-10-17 10:51:50 +0200

    Unittests now loaded from Json File

---
+++ purl_parserproject/tests/test_parser.py
@@ -0,0 +1,43 @@
+import unittest
+import json
+from purl_parser import purlparser
+
+def loadTestCases():
+    with open('purl_parserproject/tests/testcases.json') as file:
+        return json.load(file)
+
+class test_components(unittest.TestCase):
+    pass
+
+def createTestMethods(description, purl, title, namespace, name, version, qualifiers, subpath):
+        def test_method(self):
+            parsedpurl = purlparser(purl)
+            print(title)
+            self.assertEqual(parsedpurl.title, title)
+            print(namespace)
+            self.assertEqual(parsedpurl.namespace, namespace)
+            self.assertEqual(parsedpurl.name, name)
+            self.assertEqual(parsedpurl.version, version)
+            self.assertEqual(parsedpurl.qualifiers, qualifiers)
+            self.assertEqual(parsedpurl.subpath, subpath)
+            print("ok")
+        test_method.__name__ = description
+        return test_method
+
+def generate_tests():
+    for case in loadTestCases():
+        description = case['description']
+        purl = case['purl']
+        title = case['title']
+        namespace = case['namespace']
+        name = case['name']
+        version = case['version']
+        qualifiers = case['qualifiers']
+        subpath = case['subpath']
+        test_method = createTestMethods(description, purl, title, namespace, name, version, qualifiers, subpath)
+        setattr(test_components, test_method.__name__, test_method)
+
+def load_tests(loader, tests, pattern):
+    generate_tests()
+    return loader.loadTestsFromTestCase(test_components)
+
