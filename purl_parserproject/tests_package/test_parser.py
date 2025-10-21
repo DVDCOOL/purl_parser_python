49db8c2850847c850ac1034afcae38421d3d2632
obsulite <huegelig.beschattet.4s@icloud.com>
2025-10-21 16:26:06 +0200

    Qualifier as list, takes isPurl into consideration

--- purl_parserproject/tests_package/test_parser.py
+++ purl_parserproject/tests_package/test_parser.py
@@ -1,6 +1,6 @@
 import unittest
 import json
-from purl_parser import purlparser
+from purl_parser import PurlParser
 
 def loadTestCases():
     with open('purl_parserproject/tests_package/testcases.json') as file:
@@ -15,22 +15,22 @@ class test_content(unittest.TestCase):
 class test_purls(unittest.TestCase):
     pass
 
-def createTestMethods(description, purl, title, namespace, name, version, qualifiers, subpath, ispurl):
+def createTestMethods(description, purl, type, namespace, name, version, listOfQualifiers, subpath, ispurl):
         def test_method(self):
-            parsedpurl = purlparser(purl)
-            self.assertEqual(parsedpurl.title, title)
+            parsedpurl = PurlParser(purl)
+            self.assertEqual(parsedpurl.type, type)
             self.assertEqual(parsedpurl.namespace, namespace)
             self.assertEqual(parsedpurl.name, name)
             self.assertEqual(parsedpurl.version, version)
-            self.assertEqual(parsedpurl.qualifiers, qualifiers)
+            self.assertEqual(parsedpurl.listOfQualifiers, listOfQualifiers)
             self.assertEqual(parsedpurl.subpath, subpath)
-            self.assertEqual(parsedpurl.ispurl, ispurl)
+            self.assertEqual(parsedpurl.validPurl, ispurl)
         test_method.__name__ = description
         return test_method
 
 def generate_tests():
     for case in loadTestCases():
-        test_method = createTestMethods(case['description'], case['purl'], case['title'], case['namespace'], case['name'], case['version'], case['qualifiers'], case['subpath'], case['is_purl'])
+        test_method = createTestMethods(case['description'], case['purl'], case['type'], case['namespace'], case['name'], case['version'], case['listOfQualifiers'], case['subpath'], case['is_purl'])
         if case['test_type'] == "component_parsing":
             setattr(test_components, test_method.__name__, test_method)
         if case['test_type'] == "content_parsing":
@@ -45,3 +45,4 @@ def load_tests(loader, tests, pattern):
      suite.addTests(loader.loadTestsFromTestCase(test_content))
      suite.addTests(loader.loadTestsFromTestCase(test_purls))
      return suite
+
